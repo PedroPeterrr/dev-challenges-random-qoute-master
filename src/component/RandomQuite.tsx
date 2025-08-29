b1319b8167245f30cd62b57f942a7f3087215c37
asd <cabantogpeterjohnm@gmail.com>
2025-08-29 10:02:20 +0800

    Add react-hot-toast and refactor resource imports
    - Added 'react-hot-toast' for user notifications and updated error handling in API and clipboard actions to use toast messages.
    - Centralized resource imports in a new src/resources/index.tsx file and refactored RandomQuote component to use these imports. - Improved loading state UI, moved card styles to Tailwind @layer components, and added meta description to index.html.

--- src/component/RandomQuite.tsx
+++ src/component/RandomQuite.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import type { Quote } from "../api/quotes";
 import { fetchQuotes } from "../api/quotes";
-import ShareIcon from "../resources/regroup.svg";
-import LinkIcon from "../resources/link.svg";
 import '../index.css';
-import Bg from "../resources/bg-image-random-quote.png";
+
+import {
+  ShareIcon,
+  LinkIcon,
+  BackgroundImage
+} from "../resources/index";
 
 const RandomQuote = () => {
   const [quotes, setQuotes] = useState<Quote[]>([]);
@@ -31,62 +35,66 @@ useEffect(() => {
     try {
       await navigator.clipboard.writeText(`"${currentQuote.quote}" — ${currentQuote.author}`);
       setCopied(true);
-    } catch (err) {
-      console.error("Failed to copy!", err);
+    } catch (error: unknown ) {
+      toast.error((error as Error).message || "Failed to copy quote");
     }
   };
 
-  if (!currentQuote) return <p>Loading...</p>;
-
+  if (!currentQuote) {
+    return (
+      <div className="flex justify-center items-center h-64">
+        <p className="text-2xl font-bold text-white animate-pulse">
+          Loading...
+        </p>
+      </div>
+    );
+  }
   return (
-    <>
-      <div
-         className="
-    gap-3 flex flex-col text-[var(--text-color)] bg-[var(--primary-color)] 
-    rounded-xl shadow-lg text-center
-    p-4 sm:p-5 md:p-6 lg:p-7
-    w-[350px] md:w-[400px] lg:w-[500px]
-    transition-all duration-500 ease-in-out
-  "
+    <section>
+      <header
+        className="container"
         style={{
-          backgroundImage: `url(${Bg})`,
+          backgroundImage: `url(${BackgroundImage})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat'
         }}
       >
         <p className="font-bold text-[16px]">{currentQuote.author}</p>
-            <p className="flex gap-2 justify-center">
-              {currentQuote.tags.map((tags: string, index: number) => (
-                <span
-                    key={index}
-                    className="mb-2 capitalize text-[var(--secondary-color)] border rounded-full 
-                              px-3.5 py-1 text-[10px] leading-none flex items-center justify-center"
-                >
-                  {tags}
-                </span>
-              ))}
-            </p>
-          <p className="text-2xl font-light mb-2">"{currentQuote.quote}"</p>
-      </div>
 
-        <div className="flex justify-center gap-4 mt-4">
-          <button
-            onClick={getNewQuote}
-            className="px-4 py-2 bg-[var(--primary-color)] text-white rounded-xl hover:bg-[var(--primary-hover)]"
-          >
-            Random
-            <img src={ShareIcon} alt="Share" className="inline-block ml-2" />
-          </button>
-          <button
-            onClick={copyQuote}
-            className="px-4 py-2 bg-[var(--primary-color)] text-white rounded-xl hover:bg-[var(--primary-hover)]"
-          >
-            {copied ? "Copied!" : "Share"}
-            <img src={LinkIcon} alt="Link" className="inline-block ml-2" />
-          </button>
-        </div>
-    </>
+        <p className="flex gap-2 justify-center">
+          {currentQuote.tags.map((tags: string, index: number) => (
+            <span
+                key={index}
+                className="mb-2 capitalize text-[var(--secondary-color)] border rounded-full 
+                          px-3.5 py-1 text-[10px] leading-none flex items-center justify-center"
+            >
+              {tags}
+            </span>
+          ))}
+        </p>
+
+        <p className="text-2xl font-light mb-2">"{currentQuote.quote}"</p>
+      </header>
+
+      <div className="flex justify-center gap-4 mt-4">
+        <button
+          onClick={getNewQuote}
+          className="px-4 py-2 bg-[var(--primary-color)] text-white rounded-xl hover:bg-[var(--primary-hover)]"
+        >
+          Random
+          <img src={ShareIcon} alt="Share" className="inline-block ml-2" />
+        </button>
+        
+        <button
+          onClick={copyQuote}
+          className="px-4 py-2 bg-[var(--primary-color)] text-white rounded-xl hover:bg-[var(--primary-hover)]"
+        >
+          {copied ? "Copied!" : "Share"}
+          <img src={LinkIcon} alt="Link" className="inline-block ml-2" />
+        </button>
+      </div>
+    </section>
   );
 };
 
---
+++ src/resources/index.tsx
@@ -0,0 +1,9 @@
+import BackgroundImage from './bg-image-random-quote.png';
+import LinkIcon from './link.svg';
+import ShareIcon from './regroup.svg';
+
+export {
+    BackgroundImage,
+    LinkIcon,
+    ShareIcon
+}
